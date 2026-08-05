18a3a6c4ab9bf3f5ff7455cc9972631691276b8c
muhammed ansar <ansarbinkunhimarakar@gmail.com>
2026-08-05 13:54:21 +0530

    style: set light mode as default application theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <ReactQueryProvider>{children}</ReactQueryProvider>
         </ThemeProvider>
       </body>
